776c730cbcb2a2e0de837264549e21d8c8524ccf
Thomas Seilund <tps@netmaster.dk>
2019-05-23 11:09:59 +0200

    first run of digits_extract.py

--- digits_extract.py
+++ digits_extract.py
@@ -1,7 +1,32 @@
+import os
 import sys
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+if len(sys.argv) != 7:
+    print("call with:")
+    print("scoreboard image")
+    print("match template of horizontal digit part")
+    print("match template of vertical digit part")
+    print("threshold for template matching on scoreboard image")
+    print("factor for enlargement of digit part")
+    print("output image")
+    print("ie. python3 ./digits_extract.py scoreboard/venue1/scoreboard.png scoreboard/venue1/horizontal.png scoreboard/venue1/vertical.png 0.85 0.3 temp/res.png && display temp/res.png")
+    exit()
+
+# test files exist
+if not os.path.isfile(sys.argv[1]):
+    print("can't find file " + sys.argv[1])
+    exit()
+if not os.path.isfile(sys.argv[2]):
+    print("can't find file " + sys.argv[2])
+    exit()
+if not os.path.isfile(sys.argv[3]):
+    print("can't find file " + sys.argv[3])
+    exit()
+
+
 
 img_rgb = cv2.imread(sys.argv[1])
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -20,8 +45,6 @@ res = cv2.matchTemplate(img_gray,template_1,cv2.TM_CCOEFF_NORMED)
 threshold = float(sys.argv[4]) # 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-    # cv2.rectangle(img_canvas, (pt[0] /*- hH*/, pt[1]), (pt[0] + wH /*+ hH*/, pt[1] + hH), (0,0,0), -1)
-    # cv2.rectangle(img_canvas, (pt[0] - wV, pt[1]), (pt[0] + wH + wV, pt[1] + hH), (0,0,0), -1)
     cv2.rectangle(img_canvas, (pt[0] - expand, pt[1]), (pt[0] + wH + expand, pt[1] + hH), (0,0,0), -1)
     print(pt[0], pt[1])
 
@@ -30,9 +53,7 @@ res = cv2.matchTemplate(img_gray,template_2,cv2.TM_CCOEFF_NORMED)
 threshold = float(sys.argv[4]) # 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-    # cv2.rectangle(img_canvas, (pt[0], pt[1] /*- wV*/), (pt[0] /*+ wV*/, pt[1] + hV + wV), (0,0,0),-1)
-    # cv2.rectangle(img_canvas, (pt[0], pt[1] - hH), (pt[0] + wV, pt[1] + hV + hH), (0,0,0),-1)
     cv2.rectangle(img_canvas, (pt[0], pt[1] - expand), (pt[0] + wV, pt[1] + hV + expand), (0,0,0),-1)
     print(pt[0], pt[1])
 
-cv2.imwrite('res.png',img_canvas)
+cv2.imwrite(sys.argv[6],img_canvas)
